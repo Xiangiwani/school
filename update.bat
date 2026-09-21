d1a6dcd46d04c26492e6840f9ef659541fd0d46d
xiang <168374233+Xiankenv2@users.noreply.github.com>
2026-09-21 13:45:44 +0200

    test

---
+++ update.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set /p info=update info ?
+set /p branch=Welke branch? (Enter = main): 
+if "%branch%"=="" set branch=main
+
+
+git add .
+git commit -m " %info% "
+git puch origin %branch%
